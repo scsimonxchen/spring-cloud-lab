33def2b799ff0dec3bbd0c9e5f5f477574de0e51
Simon Chen <scsimonxchen@gmail.com>
2018-02-21 22:44:13 +0800

    v0.5

--- spring-cloud-config-client/src/main/java/com/spring/cloud/basic/config/client/controller/Controller.java
+++ spring-cloud-config-client/src/main/java/com/spring/cloud/basic/config/client/controller/Controller.java
@@ -1,9 +1,10 @@
 package com.spring.cloud.basic.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class Controller {
 

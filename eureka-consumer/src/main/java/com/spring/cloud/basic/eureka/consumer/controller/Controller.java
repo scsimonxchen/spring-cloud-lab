93deb2f0419acd85011a7fa2c3a2d0034dcb3941
Simon Chen <scsimonxchen@gmail.com>
2018-02-28 11:58:01 +0800

    add consul

--- eureka-consumer/src/main/java/com/spring/cloud/basic/eureka/consumer/controller/Controller.java
+++ eureka-consumer/src/main/java/com/spring/cloud/basic/eureka/consumer/controller/Controller.java
@@ -2,6 +2,7 @@ package com.spring.cloud.basic.eureka.consumer.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +16,11 @@ public class Controller {
 	@Autowired
 	private ProductService service;
 
-	@RequestMapping(value="loadRibbonServiceDefaultProductSet",method=RequestMethod.GET)
-	public List<Product> loadRibbonServiceDefaultProductSet(){
-		List<Product> pList =service.loadRibbonServiceDefaultProductSet();
+	//private static final Logger LOG = Logger.getLogger(Controller.class.getName());
+
+	@RequestMapping(value = "loadRibbonServiceDefaultProductSet", method = RequestMethod.GET)
+	public List<Product> loadRibbonServiceDefaultProductSet() {
+		List<Product> pList = service.loadRibbonServiceDefaultProductSet();
 		return pList;
 	}
 }
